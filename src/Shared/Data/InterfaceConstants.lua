1e9354799a13de716216b29e697ea6d48ab6e644
unknown <dog456pop@gmail.com>
2019-08-26 15:49:58 -0700

    Fixed dependency error in UI system

---
+++ src/Shared/Data/InterfaceConstants.lua
@@ -0,0 +1,13 @@
+return {
+	ScalingBoundaries = {
+		MIN_SIZE = Vector2.new(720, 720),
+		MAX_SIZE = Vector2.new(1080, 1920)
+	},
+
+	LayoutTypes = {
+		DESKTOP = "DESKTOP",
+		GAMEPAD = "GAMEPAD"
+	},
+
+	ROOT_NAME = "GameUI"
+}
