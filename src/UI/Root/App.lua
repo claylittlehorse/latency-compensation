7967e00e8f3ff0561999dbdacdf1808f57bf7194
unknown <dog456pop@gmail.com>
2019-08-26 15:52:12 -0700

    Removed Layout & language providers

--- src/UI/Root/App.lua
+++ src/UI/Root/App.lua
@@ -7,18 +7,14 @@ local import = require(game.ReplicatedStorage.Lib.Import)
 local Roact = import "Roact"
 local RoactRodux = import "RoactRodux"
 
-local PingDisplay = import "Components/PingDisplay"
-
-local LayoutProvider = import "../LayoutProvider"
--- local LanguageProvider = import "../LanguageProvider"
+local PingDisplay = import "UI/Components/PingDisplay"
 
 local function App(store)
 	return Roact.createElement(RoactRodux.StoreProvider, { store = store }, {
 		Roact.createElement("ScreenGui", {
 			ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 		}, {
-			LayoutProvider = Roact.createElement(LayoutProvider),
-			-- LanguageProvider = Roact.createElement(LanguageProvider),
+
 			PingDisplay = Roact.createElement(PingDisplay)
 		})
 	})
