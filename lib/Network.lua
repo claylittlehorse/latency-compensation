f5d7f1439b855f0f4544ce49ab90eebe1c5fc612
unknown <dog456pop@gmail.com>
2019-08-28 11:53:51 -0700

    Made Network errors better

--- lib/Network.lua
+++ lib/Network.lua
@@ -5,7 +5,7 @@ local FOLDER_NAME = "_NETWORK"
 
 local REMOTE_STORAGE = ReplicatedStorage:FindFirstChild(FOLDER_NAME)
 
-local CALL_ERROR_MESSAGE = "Network functions cannot be called with ':'"
+local CALL_ERROR_MESSAGE = "%s is a static method (call with '.', not ':')"
 
 if not REMOTE_STORAGE and RunService:IsServer() then
 	REMOTE_STORAGE = Instance.new("Folder", ReplicatedStorage)
@@ -34,7 +34,7 @@ local function getRemote(eventName, isFunction)
 end
 
 function Network.createEvent(eventName, isFunction)
-	assert(eventName ~= Network, CALL_ERROR_MESSAGE)
+	assert(eventName ~= Network, CALL_ERROR_MESSAGE:format("createEvent"))
 	assert(isServer, "Events cannot be created on the client.")
 	local remoteObject = Instance.new(isFunction and "RemoteFunction" or "RemoteEvent", REMOTE_STORAGE)
 	remoteObject.Name = eventName
@@ -42,7 +42,7 @@ function Network.createEvent(eventName, isFunction)
 end
 
 function Network.hookEvent(eventName, callback)
-	assert(eventName ~= Network, CALL_ERROR_MESSAGE)
+	assert(eventName ~= Network, CALL_ERROR_MESSAGE:format("hookEvent"))
 	local remoteObject = getRemote(eventName)
 
 	local event = isClient and remoteObject.OnClientEvent or remoteObject.OnServerEvent
@@ -54,16 +54,15 @@ function Network.hookEvent(eventName, callback)
 end
 
 function Network.unhookEvent(eventName)
-	assert(eventName ~= Network, CALL_ERROR_MESSAGE)
+	assert(eventName ~= Network, CALL_ERROR_MESSAGE:format("unhookEvent"))
 	local connection = eventConnections[eventName]
 	if connection then
 		connection:Disconnect()
 	end
 end
 
-
 function Network.hookFunction(functionName, callback)
-	assert(functionName ~= Network, CALL_ERROR_MESSAGE)
+	assert(functionName ~= Network, CALL_ERROR_MESSAGE:format("hookFunction"))
 	local remoteObject = getRemote(functionName, true)
 
 	local callbackKey = isClient and "OnClientInvoke" or "OnServerInvoke"
@@ -72,25 +71,35 @@ function Network.hookFunction(functionName, callback)
 end
 
 function Network.invokeServer(functionName, ...)
-	assert(functionName ~= Network, CALL_ERROR_MESSAGE)
+	assert(isClient, "Cannot invoke server functions from server")
+	assert(functionName ~= Network, CALL_ERROR_MESSAGE:format("invokeServer"))
 	local remoteObject = getRemote(functionName)
 	return remoteObject:InvokeServer(...)
 end
 
+function Network.invokeClient(functionName, ...)
+	assert(isServer, "Cannot invoke client functions from client")
+	assert(functionName ~= Network, CALL_ERROR_MESSAGE:format("invokeClient"))
+	local remoteObject = getRemote(functionName)
+	return remoteObject:InvokeClient(...)
+end
+
 function Network.fireServer(eventName, ...)
-	assert(eventName ~= Network, CALL_ERROR_MESSAGE)
+	assert(isClient, "Cannot fire server events from server")
+	assert(eventName ~= Network, CALL_ERROR_MESSAGE:format("fireServer"))
 	local remoteObject = getRemote(eventName)
 	remoteObject:FireServer(...)
 end
 
 function Network.fireClient(eventName, client, ...)
-	assert(eventName ~= Network, CALL_ERROR_MESSAGE)
+	assert(isServer, "Cannot fire client events from client")
+	assert(eventName ~= Network, CALL_ERROR_MESSAGE:format("fireClient"))
 	local remoteObject = getRemote(eventName)
 	remoteObject:FireClient(client, ...)
 end
 
 function Network.fireAllClients(eventName, ...)
-	assert(eventName ~= Network, CALL_ERROR_MESSAGE)
+	assert(eventName ~= Network, CALL_ERROR_MESSAGE:format("fireAllClients"))
 	local remoteObject = getRemote(eventName)
 	remoteObject:FireAllClients(...)
 end
